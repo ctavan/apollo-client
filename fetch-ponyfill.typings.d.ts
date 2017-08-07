86b9440c4988633443b908c3defa1bee72fcfe0c
Christoph Tavan <dev@tavan.de>
2017-08-07 13:07:26 +0200

    Add minimal typics for fetch-ponyfill

---
+++ fetch-ponyfill.typings.d.ts
@@ -0,0 +1,7 @@
+// Type definitions for fetch-ponyfill 4.1.0
+// Project: https://github.com/qubyte/fetch-ponyfill
+
+declare module 'fetch-ponyfill' {
+  function fetchPonyfill(options?: any): any;
+  export = fetchPonyfill;
+}
